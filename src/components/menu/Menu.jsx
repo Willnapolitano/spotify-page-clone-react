24aea2e64af036bb35d51f8d10d5e8b08ef95dce
Will <wstechjunior@gmail.com>
2021-01-20 09:41:27 -0300

    Added aside menu for mobile

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Logo from '../../assets/icons/Logo.png'
 import './Menu.css'
 
 export default (props) => {
@@ -10,6 +11,31 @@ export default (props) => {
                     <span className="hamburguer"></span>
                 </div>
             </label>
+            <div className="lateral">
+            <nav>
+                <div className="item-menu">
+                    <h1>Premium</h1>
+                </div>
+                <div className="item-menu">
+                    <h1>Suporte</h1>
+                </div>
+                <div className="item-menu">
+                    <h1>Baixar</h1>
+                </div>
+                <div className="item-menu">
+                    <h1>__</h1>
+                </div>
+                <div className="item-menu item-menu-cinza">
+                    <h3>Inscrever-se</h3>
+                </div>
+                <div className="item-menu item-menu-cinza">
+                    <h3>Entrar</h3>
+                </div>
+                <div className="item-menu item-menu-logo">
+                    <img src={Logo}/>
+                </div>
+            </nav>
+            </div>  
         </div>
     )
 
